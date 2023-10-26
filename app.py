ef9f48f7be03c3e7db325f020b9af73c4917980c
juliaas2 <juliaas@al.insper.edu.br>
2023-10-26 09:07:10 -0300

    ver login/editar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
 from Mongo import Mongo
 
 app = Flask(__name__)
@@ -35,6 +36,28 @@ def cadastrar():
             return {"erro":"email ou senha incorretos"}, 400
     except Exception as e:
         return {"erro":"Desculpe tivemos um problema interno, tente novamente mais tarde. Detalhes: {}".format(str(e))}, 500
+
+@app.route('/usuarios/<usuario_id>', methods=['PUT'])
+def update_usuario(usuario_id):
+    try:
+        data = request.json
+        if not data:
+            return jsonify({"error": "Dado para atualização não fornecido!"}), 400
+        usuarios.db.usuarios.update_one({"_id": ObjectId(usuario_id)}, {"$set": data})
+        return jsonify({"message": f"Usuario {usuario_id} atualizado com sucesso!"}), 200
+    except Exception as e:
+        return {"erro":str(e)}, 500
+
+@app.route('/usuarios/<usuario_id>', methods=['GET'])
+def get_usuario(usuario_id):
+    try:
+        usuario = usuarios.db.usuarios.find_one({"_id": ObjectId(usuario_id)})
+        if usuario:
+            usuario["_id"] = str(usuario["_id"])
+            return usuario, 200
+        return jsonify({"erro": "Usuario não encontrado!"}), 404
+    except Exception as e:
+        return {"erro":str(e)}, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
